27439a52db023bef24a28ae4b07f176f5a8c00f1
joaoevidotto <50387589+joaoevidotto@users.noreply.github.com>
2019-06-03 21:35:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meu_software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string diretorio = @"C:\MEU SOFTWARE\";
+
+
+            Directory.CreateDirectory(diretorio);
+            try
+            {
+                if (!File.Exists(diretorio + "cadastro" + ".txt"))
+                {
+                    File.Create(diretorio + "cadastro" + ".txt");
+                    Console.WriteLine("Diretorio criado");
+                }
+                else
+                {
+                    Stream leitor = File.Open(diretorio + "cadastro" + ".txt", FileMode.Append);
+                    StreamWriter gravador = new StreamWriter(leitor);
+                   
+                    gravador.Close();
+                    leitor.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro!" + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
